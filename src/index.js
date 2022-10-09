904d039a6e9c599bba16bdf2e39a6cbb29d4fd8c
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2022-10-09 08:42:35 +0300

    09.10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputData = document.querySelector('#search-box');
+console.log(inputData);
+let inputName = "";
+
+inputData.addEventListener('input', countriesName)
+
+function countriesName(evt) {
+    console.log(evt.currentTarget.value);
+    inputName = evt.currentTarget.value;
+    callFetch();
+}
+
+function callFetch() {
+    fetch(`https://restcountries.com/v2/name/${inputName}?fields=name,flag`)
+    .then(responce => {
+        return responce.json();
+    })
+    .then(countries => {
+    console.log(countries);
+    })
+.catch (error => {
+    console.log(error);
+})
+}

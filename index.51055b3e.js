6cc1cd5412e0b362316f69b660a4cae782000faa
Samusev-Anton <104672550+Samusev-Anton@users.noreply.github.com>
2022-10-11 07:57:07 +0000

    Deploying to gh-pages from @ Samusev-Anton/goit-js-hw-10@1d7f4d1801de003e1756b186a4479beb769084a0 🚀

--- index.51055b3e.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#search-box");console.log(e);let o="";e.addEventListener("input",(function(e){console.log(e.currentTarget.value),o=e.currentTarget.value,fetch(`https://restcountries.com/v2/name/${o}?fields=name,flag`).then((e=>e.json())).then((e=>{console.log(e)})).catch((e=>{console.log(e)}))}));
-//# sourceMappingURL=index.51055b3e.js.map

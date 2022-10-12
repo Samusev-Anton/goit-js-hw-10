5305cac9df2ebc29c7d4eca1fc5d4bf011596c45
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2022-10-12 08:21:21 +0300

    run

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -14,22 +14,31 @@ function callFetch() {
         
 
         .then(responce => {
-        // console.log(responce.json());
+            if (!responce.ok) {
+                throw new Error()
+            }
         return responce.json();
     })
         .then(countries => {
-        // console.log(countries.length);
+            console.log(countries);
         if (countries.length === undefined) {
         removeAllChildNodes(insertBox);
         } else if (countries.length === 1) {
                     removeAllChildNodes(insertBox);
-             markUp =  countries.map(({ name, flag, capital, population, languages }) => {
+            markUp = countries.map(({ name, flag, capital, population, languages }) => {
+                // console.log(languages);
+                const languagesName = Object.values(languages);
+                let lang = ""
+                languagesName.forEach(language => {
+                    lang += language.name;
+                }).join(', ');
+                // console.log(languagesName);
         return `
   <img class="gallery__image" src="${flag}" alt="${name}" />
   <p>${name} </p>
   <p>${capital} </p>
   <p>${population} </p>
-    <p>${languages[name]} </p>` }).join('');
+    <p>${lang} </p>` }).join('');
         } else if (countries.length > 10) {
             removeAllChildNodes(insertBox);
             Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
--- src/t.js
+++
@@ -1,8 +0,0 @@
-const returnedCountries = debounce(function countriesName(evt) {
-    // console.log(evt.currentTarget.value);
-    inputName = evt.currentTarget.value;
-    callFetch();
-}, 300);
-
-
-inputData.addEventListener('input', returnedCountries);
